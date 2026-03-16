d9e13568363f52643442eb404858ee568071cc26
sssh12 <ssh0311b@naver.com>
2026-03-16 17:49:17 +0900

    fix: 수정된 여백 및 경계 스타일 적용

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${inter.className} bg-gray-50 flex h-dvh w-full overflow-hidden items-center justify-center relative md:bg-gray-100`}
       >
-        <div className="relative flex w-full h-full items-center justify-center px-3 py-3 md:px-4 md:py-4">
+        <div className="relative flex w-full h-full items-center justify-center">
           <MobileContainer>{children}</MobileContainer>
 
           <div className="hidden lg:flex absolute right-4 top-4 bottom-4 flex-col gap-4 bg-white p-5 rounded-2xl shadow-sm border border-gray-200 w-72 overflow-y-auto">
--- src/components/MobileContainer.js
+++ src/components/MobileContainer.js
@@ -1,6 +1,6 @@
 export default function MobileContainer({ children }) {
   return (
-    <div className="w-full h-full md:w-1/4 md:min-h-1/2 md:rounded-[40px] md:shadow-xl md:border-8 md:border-black bg-white overflow-hidden flex flex-col relative shrink-0">
+    <div className="w-full h-full md:w-1/4 md:min-h-1/2 md:max-h-[calc(100dvh-2rem)] md:rounded-[40px] md:shadow-xl md:border-12 md:border-black bg-white overflow-hidden flex flex-col relative shrink-0">
       <div className="flex-1 overflow-y-auto w-full relative scrollbar-none">
         {children}
       </div>
